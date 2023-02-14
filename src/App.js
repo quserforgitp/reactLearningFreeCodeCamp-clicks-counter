7825f2a76f76af79302c09807ea15e84f8b8ad18
Helios <adealumnonegligas@gmail.com>
2023-02-14 15:12:53 -0600

    logo freecc

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import freeCodeCampLogo from './imagenes/FreeCodeCamp_logo.png';
 
 function App() {
   return (
     <div className="App">
+      <div className="freecodecamp-logo-contenedor">
+        <img 
+        className="freecodecamp-logo" 
+        src={freeCodeCampLogo}
+        alt=""
+        />
+      </div>
     </div>
   );
 }
